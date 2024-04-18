2b23c20b13c1b14eb0e6bfcd5cd2cd6a86440760
Siebe <r0834360@student.thomasmore.be>
2024-04-18 14:00:31 +0200

    feat: Add a logout button to profile page

--- screens/ProfileUser.js
+++ screens/ProfileUser.js
@@ -32,54 +32,60 @@ const HomeUser = ({ navigation }) => {
                     source={require('../assets/icon.png')} />
                 <Text style={globalStyles.headerText}>Naam</Text>
             </View>
-            <View style={styles.list}>
+            <View style={styles.item}>
                 <Pressable style={styles.button} onPress={goToAccount}>
                     <Image style={styles.iconImage}
                         source={require('../assets/icon.png')} />
-                    <Text style={styles.text}>Kalender</Text>
+                    <Text style={styles.text}>Mijn Account</Text>
                 </Pressable>
             </View>
-            <View style={styles.list}>
+            <View style={styles.item}>
                 <Pressable style={styles.button} onPress={goToSettings}>
                     <Image style={styles.iconImage}
                         source={require('../assets/icon.png')} />
-                    <Text style={styles.text}>Kalender</Text>
+                    <Text style={styles.text}>Instellingen</Text>
                 </Pressable>
             </View>
-            <View style={styles.list}>
+            <View style={styles.item}>
                 <Pressable style={styles.button} onPress={goToSubscription}>
                     <Image style={styles.iconImage}
                         source={require('../assets/icon.png')} />
-                    <Text style={styles.text}>Kalender</Text>
+                    <Text style={styles.text}>Mijn abonnement</Text>
                 </Pressable>
             </View>
-            <View style={styles.list}>
+            <View style={styles.item}>
                 <Pressable style={styles.button} onPress={goToQuestions}>
                     <Image style={styles.iconImage}
                         source={require('../assets/icon.png')} />
-                    <Text style={styles.text}>Kalender</Text>
+                    <Text style={styles.text}>FAQ</Text>
                 </Pressable>
             </View>
-          
+            <View style={styles.item}>
+                <Pressable style={styles.button} onPress={logout}>
+                    <Image style={styles.iconImage}
+                        source={require('../assets/icon.png')} />
+                    <Text style={styles.text}>Logout</Text>
+                </Pressable>
+            </View>
         </SafeAreaView>
     );
 }
 const styles = StyleSheet.create({
-    list: {
+    item: {
         display: 'flex',
         alignItems: 'center',
         justifyContent: 'center',
-        height: 100,
-        marginBottom: 60,
-        marginTop: 60,
+        height: 120,
+        marginBottom: 10,
+        marginTop: 10,
     },
     button: {
         display: 'flex',
         alignItems: 'center',
         justifyContent: 'center',
         height: 100,
-        marginBottom: 60,
-        marginTop: 60,
+        marginBottom: 20,
+        marginTop: 20,
     },
     text: {
         fontSize: 16,
