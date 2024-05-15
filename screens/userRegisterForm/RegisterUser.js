6fa920de8d4acbd0e7468cda96deed616b4819ac
lienapaeps <liena@minsky.be>
2024-05-15 16:39:00 +0200

    fix css

--- screens/userRegisterForm/RegisterUser.js
+++ screens/userRegisterForm/RegisterUser.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, StyleSheet, TouchableOpacity, Image } from 'react-native';
+import { View, StyleSheet, TouchableOpacity, Image, Platform } from 'react-native';
 import ProgressBar from 'react-native-progress/Bar';
 import Step1 from './Step1.js';
 import Step2 from './Step2.js';
@@ -29,27 +29,29 @@ const RegisterUser = ({ navigation }) => {
 
   return (
     <View style={styles.container}>
-      {step < totalSteps && (
-      <ProgressBar
-        progress={(step - 1) / (totalSteps - 1)}
-        width={null}
-        style={styles.progressBar}
-        color={COLORS.green}
-        height={20}
-      />
-      )}
-      {step < totalSteps && (
-        <TouchableOpacity style={styles.backButton} onPress={handlePrevStep}>
-          <Image
-            source={require('../../assets/Back-arrow.png')}
-            style={styles.arrowImg}
-          />
-        </TouchableOpacity>
+        {step < totalSteps && (
+        <ProgressBar
+          progress={(step - 1) / (totalSteps - 1)}
+          width={null}
+          style={styles.progressBar}
+          color={COLORS.green}
+          height={20}
+        />
         )}
-      {step === 1 && <Step1 />}
-      {step === 2 && <Step2 />}
-      {step === 3 && <Step3 />}
-      {step === 4 && <Step4 navigation={navigation} />}
+        {step < totalSteps && (
+          <TouchableOpacity style={styles.backButton} onPress={handlePrevStep}>
+            <Image
+              source={require('../../assets/Back-arrow.png')}
+              style={styles.arrowImg}
+            />
+          </TouchableOpacity>
+          )}
+      <View style={styles.content}>
+        {step === 1 && <Step1 />}
+        {step === 2 && <Step2 />}
+        {step === 3 && <Step3 />}
+        {step === 4 && <Step4 navigation={navigation} />}
+      </View>
       <View style={styles.buttonContainer}>
         {/* {step > 1 && <Button title="Previous" onPress={handlePrevStep} filled />} */}
         {step < totalSteps && <Button title="Next" onPress={handleNextStep} filled/>}
@@ -61,16 +63,38 @@ const RegisterUser = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'space-around',
+  },
+  content: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    ...Platform.select({
+      ios: {
+        // backgroundColor: 'red',
+      },
+    }),
+  },
+  buttonContainer: {
+    marginHorizontal: 30, 
+    marginBottom: 20,
+    ...Platform.select({
+      ios: {
+        marginBottom: 100,
+        // backgroundColor: 'blue',
+      },
+    }),
   },
   progressBar: {
     width: '100%',
     marginTop: 20,
     backgroundColor: COLORS.veryLightOffBlack,
     borderWidth: 0,
-  },
-  buttonContainer: {
-    marginHorizontal: 30,    
-    marginBottom: 20,
+    ...Platform.select({
+      ios: {
+        marginTop: 50,
+        marginBottom: 20,
+      },
+    }),
   },
   arrowImg: {
     width: 30,
@@ -79,6 +103,11 @@ const styles = StyleSheet.create({
   backButton: {
     marginLeft: 20,
     marginTop: 10,
+    ...Platform.select({
+      ios: {
+        marginBottom: 0,
+      },
+    }),
   },
 })
 
--- screens/userRegisterForm/Step1.js
+++ screens/userRegisterForm/Step1.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, Platform } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 import { globalStyles } from '../../styles/global';
@@ -7,9 +7,9 @@ import InputField from '../../components/InputField';
 
 const Step1 = () => {
   return (
-    <SafeAreaView style={globalStyles.container}>
+    <SafeAreaView style={styles.container}>
         {/* title */}
-        <View>
+        <View style={styles.form}>
             <Text style={{...globalStyles.headerText, marginBottom: 10}}>Account maken</Text>
             <Text style={globalStyles.bodyTextMedium}>Contactgegevens</Text>
         </View>
@@ -23,5 +23,28 @@ const Step1 = () => {
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+    container: {
+      ...globalStyles.container,
+      ...Platform.select({
+        ios: {
+          padding: 0,
+          // flex: .8,
+          paddingLeft: 30,
+          paddingRight: 30,
+          // justifyContent: 'flex-start',
+        },
+      }),
+    },
+    form: {
+      ...Platform.select({
+        ios: {
+          // backgroundColor: 'red',
+          marginTop: -20,
+        },
+      }),
+    },
+})
 
 export default Step1;
