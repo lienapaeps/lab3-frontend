f86affbddfcbdbf64aec9ebf82e1a8d006fc3f05
Niels Verdickt <niels.verdickt@outlook.com>
2024-06-10 16:13:05 +0200

    feat: add owner to memberslist

--- components/FarmTabPages/Members.js
+++ components/FarmTabPages/Members.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import { View, Text, StyleSheet, FlatList} from 'react-native'
+import { View, Text, StyleSheet, FlatList, Image} from 'react-native'
 import COLORS from '../../constants/color';
 import { useState, useEffect } from 'react';
 
 import { globalStyles } from '../../styles/global';
 
 import { fetchMembersData } from '../../utils/fetchHelpers';
+import { fetchFarmDataById } from '../../utils/fetchHelpers';
+import { fetchUserDataById } from '../../utils/fetchHelpers';
 
 import Lid from '../../components/Lid';
  
 const Members = ({ route }) => {
   const { data:farmId } = route.params;
   const [ledenData, setLedenData] = useState([]);
+  const [ownerData, setOwnerData] = useState("");
+  const [ownerDataId, setOwnerDataId] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -28,17 +32,63 @@ const Members = ({ route }) => {
 
       fetchLedenData();
   }, [farmId]);
-  
+
+  useEffect(() => {
+    const fetchOwnerData = async () => {
+        try {
+            const data = await fetchFarmDataById(farmId);
+            setOwnerData(data.data.farm.owner);
+        } catch (error) {
+            console.log("Error", error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    fetchOwnerData();
+}, [farmId]);
+
+    useEffect(() => {
+        const fetchOwnerData = async () => {
+            try {
+                const data = await fetchUserDataById(ownerData);
+                setOwnerDataId(data.data.user);
+                console.log(data.data.user);
+            } catch (error) {
+                console.log("Error", error);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchOwnerData();
+    }, [ownerData]);
+
+    if (loading) {
+        return <Text>Loading...</Text>;
+    }
+
+    if (!ownerDataId) {
+        return <Text>Error loading user data</Text>;
+    }
+
   return (
       <View style={styles.container}>
-        <Text style={[globalStyles.headerText, styles.title]}>Leden</Text>
         <FlatList
                 data={ledenData}
                 keyExtractor={(item) => item._id}
+                showsVerticalScrollIndicator={false}
                 renderItem={({ item }) => (
                     <Lid item={item} />
                 )}
                 ListEmptyComponent={<Text style={styles.emptyState}>Deze boerderij heeft geen leden 🤷</Text>}
+                ListHeaderComponent={<View><Text style={[globalStyles.headerText, styles.title]}>Eigenaar</Text>
+                <View style={styles.owner}>
+                    <Image style={styles.profileImage} source={{uri: ownerDataId.profilePicture}} />
+                    <Text style={[globalStyles.headerTextMedium, globalStyles.capitalize]}>{ownerDataId.firstname} {ownerDataId.lastname}</Text>
+                </View>
+        
+                <Text style={[globalStyles.headerText, styles.title]}>Leden</Text></View>}
             />
       </View>
   )
@@ -55,10 +105,21 @@ const styles = StyleSheet.create({
       fontSize: 16,
       color: COLORS.offBlack,
       fontFamily: 'Baloo2_500Medium',
-  },
-  title: {
-    marginBottom: 20
-  }
+    },
+    title: {
+     marginBottom: 20
+    },
+    profileImage: {
+        width: 40,
+        height: 40,
+        borderRadius: 50,
+        marginRight: 20,
+    },
+    owner: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginBottom: 20
+    }
 })
 
 export default Members;
