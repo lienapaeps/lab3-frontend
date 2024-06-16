6ed14cfee9a19038cc26ea18ccb27eb6d32d275f
Siebe <r0834360@student.thomasmore.be>
2024-06-16 15:41:58 +0200

    feat: add languages options

--- screens/User/Profile/Settings/Language.js
+++ screens/User/Profile/Settings/Language.js
@@ -1,16 +1,32 @@
 import React, { useState, useEffect } from 'react'
 import { View, Text, Image, StyleSheet, TouchableOpacity, SafeAreaView, Button } from 'react-native'
-
-
 import { globalStyles } from '../../../../styles/global';
+import COLORS from '../../../../constants/color';
 
 const Language = ({ navigation, route }) => {
+    const [selectedLanguage, setSelectedLanguage] = useState('Nederlands');
+
+    const languageOptions = ['Nederlands', 'Français', 'English', 'Deutsch'];
+
+    const handleLanguageSelection = (language) => {
+        setSelectedLanguage(language);
+    }
 
     return (
         <SafeAreaView style={globalStyles.container}>
             <View style={styles.header}>
                 <Text style={globalStyles.headerText}>Taal</Text>
             </View>
+            {languageOptions.map((language, index) => (
+                <TouchableOpacity
+                    key={index}
+                    style={[styles.languageOption, selectedLanguage === language && styles.selectedLanguageOption]}
+                    onPress={() => handleLanguageSelection(language)}
+                >
+                    <Text style={globalStyles.bodyTextSemiBold}>{language}</Text>
+                    {selectedLanguage === language && <Image style={styles.notificationImage} source={require('../../../../assets/arrow-down.png')} />}
+                </TouchableOpacity>
+            ))}
         </SafeAreaView>
     );
 }
@@ -18,8 +34,25 @@ const Language = ({ navigation, route }) => {
 const styles = StyleSheet.create({
     header: {
         alignItems: 'center',
-        marginBottom: 10,
+        marginBottom: 20,
         paddingBottom: 20,
+    },
+    languageOption: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        marginBottom: 10,
+        borderRadius: 5,
+    },
+    selectedLanguageOption: {
+        backgroundColor: COLORS.lightGreen,
+    },
+    notificationImage: {
+        width: 25,
+        height: 25,
+        padding: 5,
+       
     },
 });
 
